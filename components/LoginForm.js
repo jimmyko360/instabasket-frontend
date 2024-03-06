3b690dedb346380edbb4ee03e8f929020006eb1f
Jimmy Ko <ko.jimmy09@gmail.com>
2024-03-05 19:10:36 -0500

    add user registration feature and automatic login on successful registration

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,34 +1,73 @@
+import { useState } from "react";
 import { useRef } from "react";
 import axios from "axios";
 
-export default function LoginForm(props) {
+export default function AuthModal(props) {
     const usernameInputRef = useRef();
     const passwordInputRef = useRef();
+    const confirmPasswordInputRef = useRef();
 
-    function authenticationRequest(event) {
+    const [newUser, setNewUser] = useState(false);
+
+    function login() {
+        setNewUser(false);
+    }
+
+    function register() {
+        setNewUser(true);
+    }
+
+    async function signInUser(username, password) {
+        try {
+            const res = await axios.post(
+                "http://localhost:8000/dj-rest-auth/login/",
+                {
+                    username,
+                    password,
+                }
+            );
+
+            document.cookie = "token=" + res.data.key;
+            props.authHandler();
+        } catch (error) {
+            console.log("login error:", error);
+        }
+    }
+
+    async function registerUser(username, password, confirmPassword) {
+        try {
+            await axios.post(
+                "http://localhost:8000/dj-rest-auth/registration/",
+                {
+                    username,
+                    password1: password,
+                    password2: confirmPassword,
+                }
+            );
+        } catch (error) {
+            console.log("registration error:", error);
+        }
+    }
+
+    async function submitHandler(event) {
         event.preventDefault();
 
         const username = usernameInputRef.current.value;
         const password = passwordInputRef.current.value;
+        const confirmPassword = confirmPasswordInputRef?.current?.value;
 
-        axios
-            .post("http://localhost:8000/dj-rest-auth/login/", {
-                username: username,
-                password: password,
-            })
-            .then((res) => {
-                document.cookie = "token=" + res.data.key;
-                props.loginHandler();
-            })
-            .catch((error) => {
-                console.log("login error:", error);
-            });
+        if (newUser) {
+            await registerUser(username, password, confirmPassword);
+        }
+
+        signInUser(username, password);
     }
 
     return (
         <div>
-            <h1>Login</h1>
-            <form onSubmit={authenticationRequest}>
+            <p onClick={login}>Sign In</p>
+            <p onClick={register}>Register</p>
+            <form onSubmit={submitHandler}>
                 <div>
                     <label htmlFor="username">Username</label>
                     <input
@@ -47,9 +86,29 @@ export default function LoginForm(props) {
                         ref={passwordInputRef}
                     />
                 </div>
-                <div>
-                    <input type="submit" value="Log In" />
-                </div>
+                {newUser && (
+                    <div>
+                        <label htmlFor="confirmpassword">
+                            Confirm Password
+                        </label>
+                        <input
+                            type="text"
+                            required={newUser}
+                            id="confirmpassword"
+                            ref={confirmPasswordInputRef}
+                        />
+                    </div>
+                )}
+                {!newUser && (
+                    <div>
+                        <input type="submit" value="Sign In" />
+                    </div>
+                )}
+                {newUser && (
+                    <div>
+                        <input type="submit" value="Register" />
+                    </div>
+                )}
             </form>
         </div>
     );
