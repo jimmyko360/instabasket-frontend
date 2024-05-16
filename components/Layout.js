ab17cd011bdd7ad0bad50e0514b14929a2c97832
Jimmy Ko <ko.jimmy09@gmail.com>
2024-05-15 23:57:06 -0400

    add padding around entire app

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import MainHeader from "./MainHeader";
 
 export default function Layout(props) {
     return (
-        <div className="flex flex-col m-auto max-w-screen-xl">
+        <div className="flex flex-col m-auto max-w-screen-xl p-4">
             <MainHeader />
             <main>{props.children}</main>
         </div>
